17b1373c719a363a9b8a8b941dd3cb7365fbb167
Peter Ly <peter.wt.ly@gmail.com>
2025-09-16 05:11:08 -0700

    install change nvim install scrip to use appimage

--- private_dot_local/scripts/executable_curl-install-neovim.sh
+++ private_dot_local/scripts/executable_curl-install-neovim.sh
@@ -1,4 +1,8 @@
-cd /tmp
-curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
-tar -C ~/.local/bin -xzf nvim-linux-x86_64.tar.gz
-ln -sf ~/.local/bin/nvim-linux-x86_64/bin/nvim ~/.local/bin/nvim
+#!/bin/bash
+
+path=~/.local/bin/nvim
+if [ ! -f "$path" ]; then
+  mkdir -p $(dirname "$path") 
+  curl -o "$path" -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.appimage
+  chmod +x "$path"
+fi
